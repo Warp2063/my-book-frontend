c798ddd7bb2838399ecf6e84e6b22e0984b4ceaf
warp2063 <warp2063@gmail.com>
2023-05-17 13:34:49 -0400

    Updated README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const API = "http://localhost:3001/books";
+  const [books, setBooks] = useState([]);
+  const [form, setForm] = useState({
+    name: "",
+    description: "",
+    status: "",
+  });
+  const [lastDeleted, setLastDeleted] = useState({});
+
+  async function getBooks() {
+    const res = await axios.get(API);
+    setBooks(res.data);
+  }
+
+  async function deleteBook(id) {
+    const res = await axios.delete(API + "/" + id);
+    setLastDeleted(res.data);
+    getBooks();
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    await axios.post(API, form);
+    getBooks();
+    setForm({
+      name: "",
+      description: "",
+      status: "",
+    });
+  }
+
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+    //???
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Books? Books!</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          onChange={handleChange}
+          value={form.name}
+          placeholder="Book Title"
+        />
+        <input
+          name="description"
+          onChange={handleChange}
+          value={form.description}
+          placeholder="Book Description"
+        />
+        <input
+          name="status"
+          onChange={handleChange}
+          value={form.status}
+          placeholder="Book Status?"
+        />
+        <input name="submitBtn" type="submit" />
+      </form>
+
+      <br />
+      <button onClick={getBooks}>Get Books</button>
+      <button
+        onClick={() => {
+          const { name, description, status } = lastDeleted;
+          setForm({ ...form, name, description, status });
+        }}>
+        Last Deleted
+      </button>
+      <bookBox>
+        {books.map((book) => {
+          return (
+            <div className="book">
+              <p className="title">{book.name}</p>
+              <p className="description">{book.description}</p>
+              <p>{book.status}</p>
+              <p className="idNum">{book._id}</p>
+              <p className="delete" onClick={() => deleteBook(book._id)}>
+                Delete
+              </p>
+            </div>
+          );
+        })}
+      </bookBox>
     </div>
   );
 }
